66d73ea4baf1a409c65aa23a8b058b486f525f11
bpinazmul18 <bpinazmul18@gmail.com>
2022-02-16 07:32:07 +0600

    Parsing request.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
 
 // Dependencies
 const http = require('http')
-
+const url  = require('url')
+const { StringDecoder } = require('string_decoder')
 // App object - module scaffolding
 let app = {}
 
@@ -28,7 +29,34 @@ app.createServer = () => {
 
 // Handling request and response
 app.handleHttp = (req, res) => {
-     res.end('Hello world')
+     // Parse url
+     const parsedUrl = url.parse(req.url, true)
+     const path = parsedUrl.pathname
+     const trimPath = path.replace(/^\/+|\/+$/g, "")
+     const queryStringObject = parsedUrl.query
+
+     // Method
+     const method = req.method.toLowerCase()
+
+     // Headers
+     const headersObjects = req.headers
+
+     // Decode data
+     const decoder = new StringDecoder('utf-8')
+     let originalData = ''
+
+     req.on('data', (buffer) => {
+          originalData += decoder.write(buffer)
+     })
+
+     req.on('end', () => {
+          originalData += decoder.end()
+          console.log('real data...', originalData)
+          res.end('Hello world')
+     })
+
+
+
 }
 
 // Start the server
